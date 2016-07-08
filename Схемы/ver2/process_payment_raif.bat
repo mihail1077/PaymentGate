73b33d0700128f41db7356420a877dc728a54d48
Matveev Mikhail <mikematveev@yandex.ru>
2016-07-08 11:59:01 +0300

    Новая версия ТЗ

---
+++ Схемы/ver2/process_payment_raif.bat
@@ -0,0 +1,3 @@
+set file_name=process_payment_raif
+java -jar plantuml.jar -tpng %file_name%.txt -v
+%file_name%.png
---
+++ Схемы/ver2/process_payment_ruru.bat
@@ -0,0 +1,3 @@
+set file_name=process_payment_ruru
+java -jar plantuml.jar -tpng %file_name%.txt -v
+%file_name%.png
